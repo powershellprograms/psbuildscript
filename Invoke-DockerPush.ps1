88cdf2ccabf9eafadf5a11b1093990d39aca3813
Sunil Jacob <sunil1405@gmail.com>
2019-03-04 15:35:11 +0530

    Create Invoke-DockerPush.ps1

---
+++ Invoke-DockerPush.ps1
@@ -0,0 +1,5 @@
+$ErrorActionPreference="Stop"
+Param([Parameter(Mandatory=$true)][String]$DockerImage)
+$dockerPassword = "$ENV:password"
+docker push $DockerImage
+Write-Output  "Image Pushed Successfully"
