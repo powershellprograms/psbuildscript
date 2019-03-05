499a4fde56f5adbbd10b99508e8c1afc1daff122
Sunil Jacob <sunil1405@gmail.com>
2019-03-05 11:03:08 +0530

    Update Static.Tests.ps1

--- Static.Tests.ps1
+++ Static.Tests.ps1
@@ -1,5 +1,22 @@
-Describe "static analysis" {
-    It "should not have any findings" {
-        (Invoke-ScriptAnalyzer -Path . -Recurse) | Out-String | Should -BeNullOrEmpty
-    }
+#Describe "static analysis" {
+#    It "should not have any findings" {
+#        (Invoke-ScriptAnalyzer -Path . -Recurse) | Out-String | Should -BeNullOrEmpty
+#    }
+#}
+Describe 'Check if modules are installed' {
+  it 'static analysis testing' {
+    #Invoke-ScriptAnalyzer -Path . -Recurse | ForEach-Object {$_.Length} | Should Be NullOrEmpty
+    $results = Invoke-ScriptAnalyzer -Path . -Recurse
+    $results | Should Be $null
+  }
+
+  it 'Pester is installed' {
+    $results = Get-Command -Module Pester
+    $results | Should Not Be $null
+  }
+
+  it 'Nuget is installed' {
+    $results = Get-Command -Name nuget
+    $results | Should Not Be $null
+  }
 }
